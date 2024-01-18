184b44c40de516a7dbc49d75e78f9974677da038
Wilfred Almeida <almeidawilfred642@gmail.com>
2024-01-18 11:55:29 +0530

    feat: prisma setup
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# lads
+# ldas
 
 To install dependencies:
 
@@ -11,3 +11,18 @@ To run:
 ```bash
 bun run src/index.ts
 ```
+
+### Getting started with Prisma
+Follow these steps if prisma client is not automatically generated
+
+1. Mark the initial migration as applied
+   ```
+   bunx prisma migrate resolve --applied 0_init
+   ```
+
+2. Generate Prisma Client
+   ```
+   bunx prisma generate
+   ```
+
+That's it, Prisma should work now
--- src/rpc/rpc.ts
+++ src/rpc/rpc.ts
@@ -1,12 +1,17 @@
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { Connection, PublicKey, type SignaturesForAddressOptions, type TransactionSignature } from "@solana/web3.js";
+import {
+  Connection,
+  PublicKey,
+  type SignaturesForAddressOptions,
+  type TransactionSignature,
+} from "@solana/web3.js";
 
-import config from "../config/config";
+import envConfig from "../config/envConfig";
 
-export const connection = new Connection(config.rpcUrl,{
-  wsEndpoint: config.wsUrl,
+export const connection = new Connection(envConfig.rpcUrl, {
+  wsEndpoint: envConfig.wsUrl,
 });
-export const context = createUmi(config.rpcUrl);
+export const context = createUmi(envConfig.rpcUrl);
 
 export type GetSignaturesForAddressInput = {
   address: PublicKey;
@@ -17,12 +22,18 @@ export const getSignaturesForAddress = async ({
   address,
   signaturesForAddressOptions,
 }: GetSignaturesForAddressInput) => {
-  const signatures = await connection.getSignaturesForAddress(address, signaturesForAddressOptions);
+  const signatures = await connection.getSignaturesForAddress(
+    address,
+    signaturesForAddressOptions
+  );
   return signatures;
 };
 
 export const getTransaction = async (signature: TransactionSignature) => {
-  let transaction = await connection.getTransaction(signature, { maxSupportedTransactionVersion: 0, commitment: "confirmed" });
+  let transaction = await connection.getTransaction(signature, {
+    maxSupportedTransactionVersion: 0,
+    commitment: "confirmed",
+  });
 
   if (!transaction) throw new Error("Transaction not found");
 
